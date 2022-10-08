db94b0676ea24820cf54ab329016ae38613ecd4c
Niklas Hedström <karlniklashedstrom@outlook.com>
2022-10-08 11:01:59 +0200

    add path and variables management

--- scb.js
+++ scb.js
@@ -1,40 +1,86 @@
 const axios = require('axios');
 
 class SCB {
+    #paths
+    url
 
     constructor(lang) {
-        this.leng = arguments
-        this.url = "http://api.scb.se/OV0104/v1/doris/" + lang + "/ssd/BE/BE0101/BE0101A/BefolkningNy"
+        if(lang === undefined) {
+            throw new Error('SCB needs a language string paramter to be instantiated');
+          }
+        this.#paths = Object.values(arguments).slice(1)
+        this.url = "http://api.scb.se/OV0104/v1/doris/" + lang + "/ssd"
     }
 
     async getInfo() {
         const config = {
             method: 'get',
-            url: this.url
+            url: this.url + "/" + this.#paths.join("/")
         }
         let res = await axios(config)
-        console.log(JSON.stringify(res.data))
+        return res.data
     }
 
-    goDown(){
-
+    moveDown(){
+        this.#paths = this.#paths.concat(Object.values(arguments))
     }
 
-    goUp(){
-
+    moveUp(steps){
+        this.#paths = steps === undefined ? this.#paths.slice(0, -1) : this.#paths.slice(0, -steps)
     }
 
-    getVariables(){
-
+    getCurrentPath = function(){
+        return this.url + "/" + this.#paths.join("/")
     }
 
-    getVariablesCodes(){
+    //getTitle()
 
+    async isLeafNode(response){
+        return response["variables"] !== undefined
     }
 
-    getVariableCodeValues(code){
-
+    async getVariables(){
+        const response = await this.getInfo()
+        if(!this.isLeafNode(response)){
+            throw new Error("Current path is not a leaf node");
+        }
+        let variables = response["variables"];
+        let values = {}
+        for(let i = 0; i < variables.length; i++ ){
+            for(let j= 0; j < variables[i]["valueTexts"].length; j++){
+                values[variables[i]["text"]] = variables[i][["valueTexts"]]
+            }
+        }
+        return values
     }
+
+    async getVariablesCodes(){
+        const response = await this.getInfo()
+        if(!this.isLeafNode(response)){
+            throw new Error("Current path is not a leaf node");
+        }
+        let variables = response["variables"];
+        let variableCodeValues = []
+        for(let i = 0; i < variables.length; i++ ){
+                variableCodeValues.push(variables[i]["text"])
+        }
+        return variableCodeValues
+    }
+
+
+    async getVariableValuesFromText(text){
+        const response = await this.getInfo()
+        if(!this.isLeafNode(response)){
+            throw new Error("Current path is not a leaf node");
+        }
+        let variables = response["variables"];
+        for(let i = 0; i < variables.length; i++ ){
+            if(variables[i]["text"] === text){
+                return variables[i]["values"]
+            }
+        }
+    }
+
 
     setQuery(){
 
@@ -52,5 +98,12 @@ class SCB {
 
     }
 
+    getUrl(){
 
-  }
+    }
+
+}
+
+module.exports = {
+    SCB
+};
